85e5e22555c9ada269108cd8a76dbb31c8a6f0b4
Joey Smith <jsmith@webinertia.net>
2023-07-27 01:02:53 -0500

    Added PageManager Single temp route for testing Signed-off-by: Joey Smith <jsmith@webinertia.net>
    Signed-off-by: Joey Smith <jsmith@webinertia.net>

--- config/config.php
+++ config/config.php
@@ -14,6 +14,7 @@ $cacheConfig = [
 ];
 
 $aggregator = new ConfigAggregator([
+    \PageManager\ConfigProvider::class,
     \Laminas\Hydrator\ConfigProvider::class,
     \Laminas\Db\ConfigProvider::class,
     \PhpMiddleware\PhpDebugBar\ConfigProvider::class,
---
+++ src/PageManager/src/ConfigProvider.php
@@ -0,0 +1,71 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PageManager;
+
+/**
+ * The configuration provider for the PageManager module
+ *
+ * @see https://docs.laminas.dev/laminas-component-installer/
+ */
+class ConfigProvider
+{
+    /**
+     * Returns the configuration array
+     *
+     * To add a bit of a structure, each section is defined in a separate
+     * method which returns an array with its configuration.
+     */
+    public function __invoke() : array
+    {
+        return [
+            'dependencies' => $this->getDependencies(),
+            'templates'    => $this->getTemplates(),
+            'routes'       => $this->getRoutes(),
+        ];
+    }
+
+    /**
+     * Returns the container dependencies
+     */
+    public function getDependencies() : array
+    {
+        return [
+            'invokables' => [
+            ],
+            'factories'  => [
+               Handler\PageHandler::class => Handler\PageHandlerFactory::class,
+            ],
+            'delegators' => [
+                Application::class => [
+                    ApplicationConfigInjectionDelegator::class,
+                ],
+            ],
+        ];
+    }
+
+    public function getRoutes(): array
+    {
+        return [
+            [
+                'path'            => '/{title:[a-zA-Z]+}',
+                'name'            => 'page',
+                'middleware'      => Handler\PageHandler::class,
+                'allowed_methods' => ['GET'],
+            ],
+        ];
+    }
+
+    /**
+     * Returns the templates configuration
+     */
+    public function getTemplates() : array
+    {
+        return [
+            'paths' => [
+                'page-manager' => [__DIR__ . '/../templates/page-manager'],
+            ],
+        ];
+    }
+}
