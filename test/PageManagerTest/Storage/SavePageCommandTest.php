3e4aab990408a31c4bf6d2f7f0b0dfbfbe1bf490
Joey Smith <jsmith@webinertia.net>
2023-08-22 23:19:52 -0500

    Adding SavePageCommandTest Signed-off-by: Joey Smith <jsmith@webinertia.net>
    Signed-off-by: Joey Smith <jsmith@webinertia.net>

---
+++ test/PageManagerTest/Storage/SavePageCommandTest.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PageManagerTest\Storage;
+
+use PageManager\Storage\PageEntity;
+use PageManager\Storage\SavePageCommand;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+
+final class SavePageCommandTest extends TestCase
+{
+    public function testSavePageCommandAcceptsPageEntityAsArgument(): void
+    {
+        $command = new SavePageCommand(
+            new PageEntity()
+        );
+        $this->assertInstanceOf(PageEntity::class, $command->entity);
+    }
+}
